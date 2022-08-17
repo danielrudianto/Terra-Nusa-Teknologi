431aa91ecf102a3fedb13b8b5982a8e94d7ecde6
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2022-08-17 09:28:05 +0700

    Initial commit

---
+++ app/Views/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PT Terra Nusa Teknologi | IT Solutions</title>
+    <meta name="description" content="PT Terra Nusa Teknologi, perusahaan yang bergerak di bidang pengembangan piranti lunak. Berdiri sejak 2022 berlokasi di Kota Bekasi, Jawa Barat, kami hadir untuk membantu merancang, membangun, mengimplementasikan, serta memelihara sistem manajemen perusahaan - perusahaan baik di skala UMKM hingga menengah.">
+    <meta name="keywords" content="PT Terra Nusa Teknologi, Android application, Web application, IT Solutions">
+    <link rel="canonical" href="<?= base_url() ?>">
+    <meta name="robots" content="index, follow" />
+    <meta name="author" content="xunny.id">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
+    <link rel="stylesheet" href="<?= base_url() ?>/css/dark-style.css">
+</head>
+
+<body>
+    <nav class="container sticky-top">
+        <div class="row py-2">
+            <div class="col-lg-6">
+                <a class="navbar-brand" href='<?= base_url() ?>'>
+                    <img src='<?= base_url() ?>/assets/LogoLight.png' title="Logo PT Terra Nusa Teknologi" alt="Logo PT Terra Nusa Teknologi">
+                </a>
+            </div>
+            <div class="col-lg-6">
+                <ul>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="container">
+            <div class="row mt-3 justify-content-center">
+                <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-12 py-5 order-lg-1 order-2">
+                    <h1>Menghubungkan Ide Anda <strong>Tanpa Batas</strong><br><span>Dengan Teknologi</span></h1>
+                    <p>Di era teknologi 4.0, pandangan kita terhadap bisnis dan cara berinteraksi dengan konsumen telah berubah. PT Terra Nusa Teknologi hadir untuk menjawab kebutuhan tersebut, dengan personel yang terpercaya dan berpengalaman, PT. Terra Nusa Teknologi berkomitmen untuk membantu anda dalam melakukan digitalisasi perusahaan. Mari menyongsong era teknologi 4.0 bersama kami !</p>
+
+                    <button type="button" aria-label="Learn more button">Mulai</button>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-12 py-5 order-lg-2 order-1">
+                    <img src="<?= base_url() ?>/assets/Hero.png" class="w-100" alt="Hero image" title="Technology Illustration">
+                </div>
+            </div>
+        </div>
+    </header>
+    <section id="about" class="py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-12">
+                    <h2>Tentang Kami</h2>
+                    <h3>Kami menyediakan solusi IT untuk kebutuhan operasional perusahaan anda</h3>
+
+                    <p>PT Terra Nusa Teknologi adalah perusahaan teknologi informasi yang bergerak di pengembangan piranti lunak seperti aplikasi berbasis web, aplikasi mobile, ataupun aplikasi desktop, kami juga menawarkan jasa pemeliharaan sistem informasi. Secara resmi didirikan pada tahun 2022 di Kota Bekasi, perusahaan kami telah beroperasi dalam merancang, membangun, mengimplementasikan dan memelihara sistem manajemen kepada beberapa perusahaan dari skala kecil sampai skala menengah.</p>
+                    <p>Kami mengutamakan kerahasiaan data anda dan perusahaan anda, profesionalitas adalah nilai jual kami dalam melayani klien kami.</p>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-xs-12">
+                    <div class="swiper" id='service-swipe'>
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="card">
+                                    <h4>Business</h4>
+                                    <ul>
+                                        <li>
+                                            <h5>Digital and IT Advisory</h5>
+                                        </li>
+                                        <li>
+
+                                            <h5>Development Consultation</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Database Consultation</h5>
+                                        </li>
+                                        <li>
+                                            <h5>System Monitoring</h5>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="card">
+                                    <h4>Development</h4>
+                                    <ul>
+                                        <li>
+                                            <h5>IOS & Android Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Web Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Search Engine Optimization</h5>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="card">
+                                    <h4>Development</h4>
+                                    <ul>
+                                        <li>
+                                            <h5>IOS & Android Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Web Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Search Engine Optimization</h5>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="card">
+                                    <h4>Development</h4>
+                                    <ul>
+                                        <li>
+                                            <h5>IOS & Android Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Web Development</h5>
+                                        </li>
+                                        <li>
+                                            <h5>Search Engine Optimization</h5>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id='tech' class='py-5'>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Spesialisasi Kami</h2>
+                    <div class="swiper" id='tech-swipe'>
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <img src='<?= base_url() ?>/assets/android.png' alt='Android logo' title='Android logo'>
+                            </div>
+                            <div class="swiper-slide">
+                                <img src='<?= base_url() ?>/assets/angular.png' alt='Angular logo' title='Angular logo'>
+                            </div>
+                            <div class="swiper-slide">
+                                <img src='<?= base_url() ?>/assets/gcloud.png' alt='Google Cloud Platform logo' title='Google Cloud Platform logo'>
+                            </div>
+                            <div class="swiper-slide">
+                                <img src='<?= base_url() ?>/assets/ios.png' alt='iOS logo' title='iOS logo'>
+                            </div>
+                            <div class="swiper-slide">
+                                <img src='<?= base_url() ?>/assets/nodejs.png' alt='Node JS logo' title='Node JS logo'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id='solution' class='py-5'>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Solusi Unggulan</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script type="text/javascript">
+        const techSwipe = new Swiper('#tech-swipe', {
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 10,
+            centeredSlides: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 1,
+                    spaceBetween: 50,
+                },
+                992: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 50,
+                }
+            },
+            keyboard: {
+                enabled: true
+            },
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+        });
+
+        const serviceSwipe = new Swiper('#service-swipe', {
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 10,
+            centeredSlides: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 1.5,
+                    spaceBetween: 50,
+                },
+                992: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                }
+            },
+            keyboard: {
+                enabled: true
+            },
+        });
+    </script>
+</body>
