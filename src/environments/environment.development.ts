a8800bd66b2876b516e29513aa8ddab2e5e50b6a
danielrudianto <45452945+danielrudianto@users.noreply.github.com>
2023-11-18 22:05:38 +0700

    Update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // apiUrl: 'https://terranusa.tech/api',
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://terranusa.tech/api',
+  // apiUrl: 'http://localhost:3000/api',
 };
